545c7b7667ed50b6bf1404ea251d2c8238ae6f16
voronin.k8 <voronin.k8@wildberries.work>
2023-10-23 02:34:51 +0700

    init

--- hw_asr/model/deepspeech_model.py
+++ hw_asr/model/deepspeech_model.py
@@ -3,58 +3,60 @@ import torch.nn.functional as F
 from torch.nn import Sequential
 
 from hw_asr.base import BaseModel
+from hw_asr.model.GRU import GRU
+from hw_asr.model.Conv2ReLU import Conv2dReLU
 
 
-class Conv2dReLU(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, stride, dropout=0.1, padding='same'):
-        super(Conv2dReLU, self).__init__()
-        # padding = int((kernel_size - 1) / 2)
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(
-                in_channels, out_channels, kernel_size, stride=stride, padding=padding),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Dropout(dropout)
-        )
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(
-                in_channels, out_channels, kernel_size, stride=stride, padding=padding),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(),
-            nn.Dropout(dropout)
-        )
-
-    def forward(self, x):
-        res = x
-        x = self.conv1(x)
-        x = self.conv2(x)
-        return res + x
-
-
-class GRU(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers=1, batch_first=True, dropout=0.1):
-        super(GRU, self).__init__()
-        self.input_size = input_size
-        self.hidden_size = hidden_size
-        self.num_layers = num_layers
-        self.batch_first = batch_first
-        self.dropout = nn.Dropout(dropout)
-        self.gru = nn.GRU(
-            input_size=input_size,
-            hidden_size=hidden_size,
-            num_layers=num_layers,
-            batch_first=batch_first,
-            bidirectional=True
-        )
-        self.batch_norm = nn.BatchNorm1d(hidden_size * 2)
-
-    def forward(self, x):
-        # x = self.batch_norm(self.dropout(x))
-        x = F.leaky_relu(self.dropout(x))
-        # x = nn.utils.rnn.pack_padded_sequence(x, lengths.cpu(), batch_first=self.batch_first)
-        x, _ = self.gru(x)
-        # x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=self.batch_first)
-        return x
+# class Conv2dReLU(nn.Module):
+#     def __init__(self, in_channels, out_channels, kernel_size, stride, dropout=0.1, padding='same'):
+#         super(Conv2dReLU, self).__init__()
+#         # padding = int((kernel_size - 1) / 2)
+#         self.conv1 = nn.Sequential(
+#             nn.Conv2d(
+#                 in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+#             nn.BatchNorm2d(out_channels),
+#             nn.ReLU(),
+#             nn.Dropout(dropout)
+#         )
+#         self.conv2 = nn.Sequential(
+#             nn.Conv2d(
+#                 in_channels, out_channels, kernel_size, stride=stride, padding=padding),
+#             nn.BatchNorm2d(out_channels),
+#             nn.ReLU(),
+#             nn.Dropout(dropout)
+#         )
+#
+#     def forward(self, x):
+#         res = x
+#         x = self.conv1(x)
+#         x = self.conv2(x)
+#         return res + x
+#
+#
+# class GRU(nn.Module):
+#     def __init__(self, input_size, hidden_size, num_layers=1, batch_first=True, dropout=0.1):
+#         super(GRU, self).__init__()
+#         self.input_size = input_size
+#         self.hidden_size = hidden_size
+#         self.num_layers = num_layers
+#         self.batch_first = batch_first
+#         self.dropout = nn.Dropout(dropout)
+#         self.gru = nn.GRU(
+#             input_size=input_size,
+#             hidden_size=hidden_size,
+#             num_layers=num_layers,
+#             batch_first=batch_first,
+#             bidirectional=True
+#         )
+#         self.batch_norm = nn.BatchNorm1d(hidden_size * 2)
+#
+#     def forward(self, x):
+#         # x = self.batch_norm(self.dropout(x))
+#         x = F.leaky_relu(self.dropout(x))
+#         # x = nn.utils.rnn.pack_padded_sequence(x, lengths.cpu(), batch_first=self.batch_first)
+#         x, _ = self.gru(x)
+#         # x, _ = nn.utils.rnn.pad_packed_sequence(x, batch_first=self.batch_first)
+#         return x
 
 
 class DeepSpeechModel(BaseModel):
