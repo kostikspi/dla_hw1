da405810770ce0becc289b7d2c4d64793b70b89a
voronin.k8 <voronin.k8@wildberries.work>
2023-10-24 18:58:54 +0700

    kaggle config

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,13 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
+from hw_asr.augmentations.wave_augmentations.AddColoredNoise import AddColoredNoise
+from hw_asr.augmentations.wave_augmentations.ApplyImpulseResponse import ApplyImpulseResponse
+from hw_asr.augmentations.wave_augmentations.PitchShift import PitchShift
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "AddBackgroundNoise",
+    "AddColoredNoise",
+    "ApplyImpulseResponse",
+    "PitchShift"
 ]
